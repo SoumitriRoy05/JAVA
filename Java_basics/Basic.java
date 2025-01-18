659cb1dfcd25cbcdfb9da34d4fcd924b584cb777
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-18 20:51:05 +0530

    Add files via upload

---
+++ Java_basics/Basic.java
@@ -0,0 +1,12 @@
+package Java_basics;
+
+public class Basic {
+    public static void main(String[] args) {
+        int x=500;
+        System.out.println(x);
+          x=400;   
+        System.out.println(x);
+    }
+   
+
+}
