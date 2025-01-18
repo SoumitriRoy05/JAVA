79c7ec57ef685e54282b5fc13bd021fd56312d4d
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-18 20:47:42 +0530

    Add files via upload

---
+++ Java_basics/Hello_world2.java
@@ -0,0 +1,8 @@
+package Java_basics;
+
+public class Hello_world2 {
+    public static void main(String[] args) {
+        System.out.println("Good morning");
+        System.out.println("Soumitri Roy");
+    }
+}
