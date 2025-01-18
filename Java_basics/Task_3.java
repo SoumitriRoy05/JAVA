5026165220343ba04b745c204e26bbcfea33950a
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-18 20:55:12 +0530

    Add files via upload

---
+++ Java_basics/Task_3.java
@@ -0,0 +1,14 @@
+package Java_basics;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        //calculating percentage of 5 subjects
+        double x1=90;//maths
+        double x2=91;//chemistry
+        double x3=92;//english
+        double x4=93;//physics
+        double x5=94;//it
+        double y= (x1+x2+x3+x4+x5)/5;
+        System.out.println("The percentage of 5 subjects: "+y);
+    }
+}
