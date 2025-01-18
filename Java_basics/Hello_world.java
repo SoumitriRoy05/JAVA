f8bedd097ff6588dd09d2c56bcd1e64ed10120a5
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-18 20:46:49 +0530

    Add files via upload

---
+++ Java_basics/Hello_world.java
@@ -0,0 +1,8 @@
+package Java_basics;
+
+public class Hello_world {
+    public static void main(String[] args) {
+       System.out.print("Hello World!");
+        System.out.print("Good Morning!");
+    }
+}
