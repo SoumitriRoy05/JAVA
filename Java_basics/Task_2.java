f8e1f755fe79391b0bef7b12f71ea2ea6e64c562
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-18 20:54:48 +0530

    Add files via upload

---
+++ Java_basics/Task_2.java
@@ -0,0 +1,11 @@
+package Java_basics;
+
+public class Task_2 {
+public static void main(String[] args) {
+// sum of 2 numbers
+int x=6;
+int y=24;
+System.out.println("Sum is: " +(x+y));
+}
+    
+}
