c30d9c7a967c656de29fcf76d7ac55502ece3f8e
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-18 20:55:42 +0530

    Add files via upload

---
+++ Java_basics/Task_4.java
@@ -0,0 +1,11 @@
+package  Java_basics;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        //area of circle
+        double radius=5;
+        double pi=3.14;
+        double area= pi*radius*radius;
+        System.out.println("Area of a circle: "+area);
+    }
+}
