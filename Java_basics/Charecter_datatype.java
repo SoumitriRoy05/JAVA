e6971a23bc333851363cb05351f50f2ad742692b
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-19 19:14:31 +0530

    Add files via upload

---
+++ Java_basics/Charecter_datatype.java
@@ -0,0 +1,11 @@
+package Java_basics;
+import java.util.Scanner;
+public class Charecter_datatype {
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+        char ch='S';
+        System.out.println(ch);
+        int x=(int)ch;
+        System.out.println(x);
+    }
+}
