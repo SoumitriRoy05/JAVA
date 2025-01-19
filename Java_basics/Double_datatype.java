52b54aa2c3687063c414270f5bfa098de8e2fc97
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-19 19:21:33 +0530

    Add files via upload

---
+++ Java_basics/Double_datatype.java
@@ -0,0 +1,13 @@
+package Java_basics;
+
+public class Double_datatype {
+    public static void main(String[] args) {
+        double x=5;
+        double y=2;
+        System.out.println(x/y);
+        System.out.println(5/2);
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+    }
+}
