27f0e6ecdf4f6563f85c44bc63a25d62e0dec71c
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-19 19:12:55 +0530

    Add files via upload

---
+++ Java_basics/Variables.java
@@ -0,0 +1,12 @@
+package Java_basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x=500;
+        System.out.println(x);
+        x=400;
+        System.out.println(x+1);
+        x=x+5;
+        System.out.println(x);
+    }
+}
