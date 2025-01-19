874191c11bdd8f1f65d9df79440625b9556495bb
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-19 19:19:52 +0530

    Add files via upload

---
+++ Java_basics/Multiplevariable.java
@@ -0,0 +1,13 @@
+package Java_basics;
+
+public class Multiplevariable {
+    public static void main(String[] args) {
+       int x=5, y=8,z=9;
+        System.out.println("The value of x is: " +x);
+        System.out.println("The value of y is: " + (100+y));
+     //  z=10;
+     //z=x/y;
+       System.out.println("The value of z is : "+z); 
+       System.out.println("mast Cheez " +(x+y+z));
+    }
+}
