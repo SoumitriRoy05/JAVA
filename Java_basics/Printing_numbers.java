f4a3f4c983f69047115a8ff2e824a2189899b495
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-19 19:12:02 +0530

    Add files via upload

---
+++ Java_basics/Printing_numbers.java
@@ -0,0 +1,10 @@
+package Java_basics;
+
+public class Printing_numbers {
+    public static void main(String[] args) {
+        //System.out.println("4");
+       // System.out.println("" +4);
+       //System.out.println("" +(4+3));
+       System.out.println("4+3");
+    }
+}
