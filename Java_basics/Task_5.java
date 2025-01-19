09cf0bbca90fc86c5a683244cb1a3e50d5fee862
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-19 19:05:13 +0530

    Add files via upload

---
+++ Java_basics/Task_5.java
@@ -0,0 +1,12 @@
+package Java_basics;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        //calculate simple interest
+        double principal=100;
+        double rate=10;
+        double time=2;
+        double simple_interest=(principal*rate*time)/100;
+        System.out.println("Simple Interest: "+simple_interest);
+    }
+}
