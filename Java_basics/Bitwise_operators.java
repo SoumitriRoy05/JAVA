cb9dbd8c1ba9e7ee48ed22cb517bc11fdc1ca734
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-20 19:19:09 +0530

    Add files via upload

---
+++ Java_basics/Bitwise_operators.java
@@ -0,0 +1,16 @@
+package Java_basics;
+
+public class Bitwise_operators {
+    public static void main(String[] args) {
+      int p=9;
+      int q=10;
+      System.out.println(p|q);
+      System.out.println(p&q);
+      System.out.println(p^q);
+      System.out.println(p<<1);
+      System.out.println(p<<2);
+      System.out.println(q>>1);
+      System.out.println(q>>2);
+    }
+    
+}
