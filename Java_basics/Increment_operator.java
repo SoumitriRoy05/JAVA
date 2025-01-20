f195376a3e44a51b58a7cd8716d817866e0742ae
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-20 19:21:10 +0530

    Add files via upload

---
+++ Java_basics/Increment_operator.java
@@ -0,0 +1,11 @@
+package Java_basics;
+
+public class Increment_operator {
+    public static void main(String[] args) {
+        int x=4;
+        x++;
+        System.out.println(x);
+        ++x;
+        System.out.println(x);
+    }
+}
