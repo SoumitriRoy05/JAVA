5c1e4b36f9904a150e0efcd55c28c631f5668f31
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-20 19:21:49 +0530

    Add files via upload

---
+++ Java_basics/Decrement_operators.java
@@ -0,0 +1,11 @@
+package Java_basics;
+
+public class Decrement_operators {
+    public static void main(String[] args) {
+        int x=8;
+        x--;
+        System.out.println(x);
+        --x;
+        System.out.println(x);
+    }
+}
