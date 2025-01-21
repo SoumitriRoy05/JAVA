29d5d84f43af2e7e60b0e412342bcb8e6c8078c2
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-21 20:45:07 +0530

    Add files via upload

---
+++ Loops_java/Do_while_loop.java
@@ -0,0 +1,12 @@
+package Loops_java;
+
+public class Do_while_loop {
+    public static void main(String[] args) {
+        int num=1;
+        do{
+            System.out.println(num);
+            num++;
+        }
+        while(num<=10);
+    }
+}
