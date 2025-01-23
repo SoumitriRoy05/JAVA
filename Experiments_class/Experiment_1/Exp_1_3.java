af374e842b5f15369b0d336d60abaaba93377159
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-23 20:39:56 +0530

    Add files via upload

---
+++ Experiments_class/Experiment_1/Exp_1_3.java
@@ -0,0 +1,21 @@
+package Experiments_class;
+import java.util.Scanner;
+public class Exp_1_3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of x:");
+        int x=sc.nextInt();
+        System.out.println("Enter the value of x:");
+        int y=sc.nextInt();
+        System.out.println("Arithmetic operators:");
+        System.out.println("x+y= "+(x+y));
+        System.out.println("x-y= "+(x-y));
+        System.out.println("x*y= "+(x*y));
+        System.out.println("x/y= "+(x/y));
+        System.out.println("Logical Operators:");
+        System.out.println("x==y: "+(x==y));
+        System.out.println("x>=y: "+(x>=y));
+        System.out.println("x<=y: "+(x<=y));
+        System.out.println("x!=y: "+(x!=y));
+    }
+}
