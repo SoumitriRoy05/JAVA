dc7663a407decdad27fdf2dab9c74fcbd39b2802
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-20 21:08:03 +0530

    Add files via upload

---
+++ Variable/Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int a=10,b=20,c=30;
+        System.out.println(a);
+        System.out.println(a +"," +b);
+        System.out.println(a+ "," +b +"," +c);
+    }
+}
