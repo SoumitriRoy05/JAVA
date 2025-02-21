19a55bc497eb2d5695b8d1418a68240d7bfdb4ac
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-21 23:12:24 +0530

    Add files via upload

---
+++ Variable/Q6.java
@@ -0,0 +1,9 @@
+public class Q6 {
+    public static void main(String[] args) {
+        int age=20;
+        String name="Soumitri";
+        char gender='F';
+        double height=5.6;
+        System.out.println("DETAILS: " +name +","+age+","+gender+","+height);
+    }
+}
