f0e2a08cc2dab482a1b6272bcbf8254d68691c50
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-21 23:16:10 +0530

    Update Q3.java

--- Variable/Q3.java
+++ Variable/Q3.java
@@ -1,3 +1,7 @@
+// WRITE A JAVA PROGRAM TO PRINT THE VALUE OF a,b,c IN THE FOLLOWING WAYS
+// 1. ONLY VALUE OF a
+// 2. VALUE OF BOTH a & b
+// 3. VALUE OF a,b & c
 public class Q3 {
     public static void main(String[] args) {
         int a=10,b=20,c=30;
