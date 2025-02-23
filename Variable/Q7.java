5a1d4dfc7ea25428184c5af580ed4dba9efe1494
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-23 22:52:11 +0530

    Update Q7.java

--- Variable/Q7.java
+++ Variable/Q7.java
@@ -1,3 +1,4 @@
+// WRITE A PROGRAM IN JAVA TO PRINT NAME, AGE,GENDER,HEIGHT USING A SINGLE PRINT STATEMENT AND PRINT IT IN DIFFERENT LINES
 public class Q7 {
     public static void main(String[] args) {
         String name="Soumitri";
