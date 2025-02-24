2e4ca12a65ec2bd2dac605d4e31a37e072c26bd5
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-24 21:36:42 +0530

    Add files via upload

---
+++ Arithmetic_operators/Q2.java
@@ -0,0 +1,9 @@
+// package Arithmetic_operators;
+
+public class Q2 {
+    public static void main(String[] args) {
+        float a=6,b=7,c=10;
+        float sum=a+b+c;
+        System.out.println("Sum of 3 decimal numbers:"+sum);
+    }
+}
