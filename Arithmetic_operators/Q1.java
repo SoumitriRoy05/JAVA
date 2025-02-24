14b5336104cd6a82b5430026ca13c4a7d98826b3
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-24 21:36:01 +0530

    Add files via upload

---
+++ Arithmetic_operators/Q1.java
@@ -0,0 +1,9 @@
+// package Arithmetic_operators;
+
+public class Q1 {
+   public static void main(String[] args) {
+    int a=6,b=10;
+    int sum=a+b;
+    System.out.println("Sum of 2 integers: " +sum);
+   } 
+}
