c73feb203a168fcd23aa5c64e25c973dddb6ebb5
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-26 23:33:25 +0530

    Add files via upload

---
+++ Arithmetic_operators/Q3.java
@@ -0,0 +1,9 @@
+// package Arithmetic_operators;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int a=6,b=8,c=10;
+        int avg=(a+b+c)/3;
+        System.out.println("Average:" +avg);
+    }
+}
