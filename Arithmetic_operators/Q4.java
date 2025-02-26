7c1184a0c117a37ceed77d9cbb1d6aa8d67edca4
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-26 23:34:49 +0530

    Add files via upload

---
+++ Arithmetic_operators/Q4.java
@@ -0,0 +1,10 @@
+// package Arithmetic_operators;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int a=24,b=6;
+        int quo=a/b;
+        int rem=a%b;
+        System.out.println("Qoutient:" +quo);
+    }
+}
